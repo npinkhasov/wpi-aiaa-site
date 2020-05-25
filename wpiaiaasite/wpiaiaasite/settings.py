fc7d4f8120fca3f514fd2f929ab1b6aa1b55b841
Christian M. Schrader <cmschrader@wpi.edu>
2020-05-25 18:34:43 -0400

    First Upload

--- wpiaiaasite/wpiaiaasite/settings.py
+++ wpiaiaasite/wpiaiaasite/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&&8h7t*&tecxoluh4&&dym)8ax^*2g5g^^f9hcv!m-iu&0rk7i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["aiaa.wpi.edu"]
 
 
 # Application definition
@@ -136,6 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Wiki Settings
 SITE_ID = 1
