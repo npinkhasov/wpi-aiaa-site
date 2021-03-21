48d460d06046ee78c51bab4f9d7043d554faa8cd
Max Schrader <cmschrader@wpi.edu>
2021-03-20 23:33:15 -0400

    Project Pages

--- wpiaiaasite/hprc/views.py
+++ wpiaiaasite/hprc/views.py
@@ -5,7 +5,6 @@ def index(request):
     context = {'nbar': 'home'}
     return render(request, "hprc/index.html", context)
 
-
 def sponsors(request):
     context = {'nbar': 'sponsors'}
     return render(request, "hprc/sponsors.html", context)
@@ -22,3 +21,27 @@ def team(request):
     context = {'nbar': 'team'}
     return render(request, "hprc/team.html", context)
     
+def goats(request):
+    context = {
+        'nbar': "projects",
+        "project_title": "G.O.A.T.S",
+        "lead_text": "WPI's first year competing in USLI",
+        "docs": {
+            "Proposal": "link",
+            "Preliminary Design Review": "link",
+            "Comprehensive Design Review": "link",
+            "Flight Readiness Review": "link",
+            "Post-Launch Assessment Review": "link",
+        },
+        "sponsors": {
+            "Platinum": [
+                "Jones Machine Company"
+            ],
+            "Bronze": [
+                "Test Devices Inc.",
+                "Plasma Technology Inc.",
+                "Consulting Structural Engineering Inc.",
+            ]
+        }
+    }
+    return render(request, "hprc/project.html", context)
